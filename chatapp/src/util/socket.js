c2a1e23f130ca677b123d3f4b3688dadb4841e56
DamoFD <voshall@yahoo.com>
2023-04-19 18:44:17 -0700

    added transports to socket

--- chatapp/src/util/socket.js
+++ chatapp/src/util/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
 const socket = new io('https://damion-chat-app.onrender.com', {
+    transports: ["websocket"],
     autoConnect: false,
     withCredentials: true,
 })
